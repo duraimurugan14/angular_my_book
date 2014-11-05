871496b83de6a6d0f17affdd96f02793da999b2b
duraimurugan_rajendran <duraimurugan.rajendran@object-frontier.com>
2014-11-05 12:10:38 +0530

    changes to feed delete logic

--- app/scripts/directives/feedlist.js
+++ app/scripts/directives/feedlist.js
@@ -11,10 +11,10 @@ angular.module('mybookApp')
     return {
       restrict: 'E',
       template: '<div ng-repeat = "item in items"> <br>' + 
-      				'<div ng-class-odd="odd" ng-class-even="even" class = "padding">' + 
+      				'<div  class = "padding">' + 
       					'<img class = "usrimgSize" ng-src="images/user_icon.jpeg">' + 
-       					'<label id = "feedValues"> {{item.caption}} </label>' +
-       					'<label> {{date | date:"MM-dd-yyyy HH:mm:ss"}} </label>' +
+       					'<span id = "feedElement" class = "feedValues"> {{item.caption}} </span>' +
+       					'<span class = "feedTime"> {{date | date:"MM-dd-yyyy HH:mm:ss"}} </span>' +
        					'<img class = "usrDelImg" src="images/remove_icon.jpeg" ng-click= "deleteItems(item.id)">' +
        				'</div>' +
        			'</div>' 
