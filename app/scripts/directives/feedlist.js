c64537d7ee6ee77806fd4b8d358ecf1526e9f280
duraimurugan_rajendran <duraimurugan.rajendran@object-frontier.com>
2014-11-05 14:48:54 +0530

    cleanup

--- app/scripts/directives/feedlist.js
+++ app/scripts/directives/feedlist.js
@@ -13,7 +13,7 @@ angular.module('mybookApp')
       template: '<div ng-repeat = "item in items"> <br>' + 
       				'<div  class = "padding">' + 
       					'<img class = "usrimgSize" ng-src="images/user_icon.jpeg">' + 
-       					'<span id = "feedElement" class = "feedValues"> {{item.caption}} </span>' +
+       					'<span id = "feedElement" class = "feedValues"> {{item.caption}}</span>' +
        					'<span class = "feedTime"> {{date | date:"MM-dd-yyyy HH:mm:ss"}} </span>' +
        					'<img class = "usrDelImg" src="images/remove_icon.jpeg" ng-click= "deleteItems(item.id)">' +
        				'</div>' +
